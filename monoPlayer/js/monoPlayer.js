ade020561e865598875893d6ed9e66e7e2969139
Eric S. Bishard <eric@httpjunkie.com>
2015-07-05 09:36:40 -0400

    added bower, .gitignore and normalize.css. renamed the index.html file to demo.html and added a demo.css file in css directory. Also made changes to the monoPlayer.js file and the mp3 file to show details about the track

--- monoPlayer/js/monoPlayer.js
+++ monoPlayer/js/monoPlayer.js
@@ -1,87 +1,145 @@
+/*
+	monoPlayer.js
+	Version: 0.1
+*/
 
- (function customAudio() {
-		// window.addEventListener('load', init, false);
+var monoPlayer = (function () {
+	 
+	// Initialize buttons on page load	
+	window.addEventListener('load', initButtons, false);
+	
+	// global audio variable (right now just grabs the first track)
+	var audio = document.getElementsByTagName('audio')[0];
+	
+	// variables for each button
+	var playBtn = document.getElementById('play');
+	var pauseBtn = document.getElementById('pause');
+	var stopBtn = document.getElementById('stop');
+	var muteBtn = document.getElementById('mute');
+	var muteIcon = document.getElementById('muteIcon');
+	
+	// debug/warning variables
+	var alertProblemButtons = false;
+	
+	// icon style variables
+	var muteOnClass = "icon-mute_on";
+	var muteOffClass = "icon-mute_off";
+	
+	// check to make sure elements were named correctly in html
+	function initButtons() {
 		
-		//global variables
-		var audio = document.getElementsByTagName('audio')[0];
-		
-		//get a reference to the element
-  		var playBtn = document.getElementById('play');
-		var pausBtn = document.getElementById('paus');
-		var stopBtn = document.getElementById('stop');
-		var muteBtn = document.getElementById('mute');
-		var muteIcon = document.getElementById("muteIcon")
-		
-		//check to make sure elements were named correctly for this js
 		if(playBtn != null){
-			playBtn.addEventListener('click', playAudio, false );
+			playBtn.addEventListener('click', playAudio, false);
+		} else {
+			console.log('Play button class is not named "play"');
+			alertProblemButtons = true;
 		}
-		if(pausBtn != null){
-			pausBtn.addEventListener('click', pausAudio, false );
+		
+		if(pauseBtn != null){
+			pauseBtn.addEventListener('click', pausAudio, false );
+		} else {
+			console.log('Pause button class is not named "pause"');
+			alertProblemButtons = true;
 		}
-  		if(muteBtn != null){
+		
+		if(muteBtn != null){
 			muteBtn.addEventListener('click', muteAudio, false );
+		} else {
+			console.log('Mute button class is not named "mute"');
+			alertProblemButtons = true;
 		}
+		
 		if(stopBtn != null){
 			stopBtn.addEventListener('click', stopAudio, false );
+		} else {
+			console.log('Stop button class is not named "stop"');
+			alertProblemButtons = true;
 		}
 		
-		function playAudio() {
-			if(audio.paused && !audio.muted && audio.currentTime <= 0) {
-				audio.play();
-			}
-			else if (!audio.paused && audio.muted && audio.currentTime > 0) {
-				audio.muted = false;
-			  	muteIcon.className = "icon-mute_off";
-			}
-			else if (audio.paused && !audio.muted && audio.currentTime > 0) {
-				audio.play();
-			}	
-			else if (audio.paused && audio.muted && audio.currentTime > 0) {
-				audio.play();
-				audio.muted = false;
-				muteIcon.className = "icon-mute_off";
-			}
+		if (alertProblemButtons) {
+			alert('WARNING:' + '\n' + 'Problem with monoPlayer buttons "see console logs"');
 		}
 		
-		function pausAudio() {
+		// console.log("Loaded track is: "+ Math.round(audio.duration) +" seconds long");
+		// console.log("Loaded track playback rate is: " + audio.playbackRate);
+		
+	}
+	
+	// function called by the play button
+	function playAudio() {
+		
+		if (!audio.paused && audio.muted && audio.currentTime > 0) {
+			audio.muted = false;
+		  	muteIcon.className = muteOffClass;
+		}
+		
+		else if (audio.paused && audio.muted && audio.currentTime > 0) {
+			audio.muted = false;
+			muteIcon.className = muteOffClass;
+			audio.play();
+		}
+		
+		else if (audio.paused && !audio.muted && audio.currentTime > 0) {
+			audio.play();
+		}
+		
+		if(audio.paused && !audio.muted && audio.currentTime <= 0) {
+			audio.play();
+		}
+		
+	}
+	
+	// function called by the pause button
+	function pausAudio() {
+		audio.pause();
+	}
+	
+	// function called by the mute button
+	function muteAudio() {
+		
+		if(audio.paused && !audio.muted && audio.currentTime <= 0) {
+			audio.muted = false;
+		}
+		
+		else if(!audio.paused && !audio.muted  && audio.currentTime > 0) {
+			audio.muted = true;
+			muteIcon.className = muteOnClass;
+		}
+		
+		else if(!audio.paused && audio.muted == true) {
+		  	audio.muted = false;
+		  	muteIcon.className = muteOffClass;
+		}
+		
+	}
+	
+	// function called by the stop button
+	function stopAudio() {
+		
+		if (audio.paused && audio.muted && audio.currentTime > 0) {
+			audio.currentTime = 0;
+			audio.pause();
+			audio.muted = false;
+			muteIcon.className = muteOffClass;
+		}
+		
+		else if (!audio.paused && audio.muted && audio.currentTime > 0) {
+			audio.currentTime = 0;
+			audio.pause();
+			audio.muted = false;
+		  	muteIcon.className = muteOffClass;
+		}
+		
+		else if(audio.paused && audio.currentTime > 0) {
+			audio.currentTime = 0;
 			audio.pause();
 		}
 		
-		function muteAudio() {
-			if(audio.paused && !audio.muted && audio.currentTime <= 0) {
-				audio.muted = false;
-			}
-			else if(!audio.paused && !audio.muted  && audio.currentTime > 0) {
-				audio.muted = true;
-				muteIcon.className = "icon-mute_on";
-			}
-			else if(!audio.paused && audio.muted == true) {
-			  	audio.muted = false;
-			  	muteIcon.className = "icon-mute_off";
-			}
+		else if(!audio.paused && audio.currentTime > 0){
+			audio.currentTime = 0;
+			audio.pause();
 		}
 		
-		function stopAudio() {
-			if (audio.paused && audio.muted && audio.currentTime > 0) {
-				audio.muted = false;
-			  	muteIcon.className = "icon-mute_off";
-				audio.pause();
-				audio.currentTime = 0;
-			}
-			else if(!audio.paused && audio.muted && audio.currentTime > 0) {
-				audio.muted = false;
-			  	muteIcon.className = "icon-mute_off";
-				audio.pause();
-				audio.currentTime = 0;
-			}
-			else if(audio.paused && audio.currentTime > 0) {
-				audio.pause();
-				audio.currentTime = 0;
-			}
-			else if(!audio.paused && audio.currentTime > 0){
-				audio.pause();
-				audio.currentTime = 0;
-			}
-		}
+	}
+		
  }());
